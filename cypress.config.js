b404bcc59d96edd8aba55557784790c1c953b8e3
username960528 <username960528@gmail.com>
2023-08-14 19:55:34 +0500

    cypress settings

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
     setupNodeEvents(on, config) {
+      
       // implement node event listeners here
     },
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
   },
 });
