d018ac56e3cd093f292fca78fcfd7811ddb27108
username960528 <username960528@gmail.com>
2023-08-15 14:35:08 +0500

    elements test

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
+describe('A/B spec', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:9292/')
+    cy.visit('http://127.0.0.1:9292/?optimizely_disable=true')
+    cy.contains('A/B Testing')
   })
 })
