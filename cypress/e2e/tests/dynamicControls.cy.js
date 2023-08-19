99a013657c5d9d2d333c26bb80d27202fd0d5ad9
username960528 <username960528@gmail.com>
2023-08-20 00:09:50 +0500

    dynamicControls

---
+++ cypress/e2e/tests/dynamicControls.cy.js
@@ -0,0 +1,36 @@
+describe('dynamic controls test', () => {
+    it('pases', () => {
+        cy.visit('http://127.0.0.1:9292/dynamic_controls')
+    })
+
+    /* ==== Test Created with Cypress Studio ==== */
+    it('studio', function() {
+        /* ==== Generated with Cypress Studio ==== */
+        cy.visit('http://127.0.0.1:9292/dynamic_controls');
+        cy.get('#checkbox > input').should('not.be.checked');
+        cy.get('#checkbox').should('be.visible');
+        cy.get('#checkbox > input').check();
+        cy.get('#checkbox > input').should('be.checked');
+        cy.get('#checkbox-example > button').click();
+        cy.get('input').should('be.disabled');
+        cy.get('#input-example > button').click();
+        cy.get('input').should('be.enabled');
+        cy.get('[data-cy="bla"]').type('qq');
+        cy.get('#input-example > button').should('have.text', 'Disable');
+        cy.get('#input-example').click();
+        cy.get('#input-example > button').click();
+        cy.get('input').should('be.disabled');
+        cy.get('#checkbox-example > button').click();
+        cy.get('body').click();
+        cy.get('body').click();
+        cy.get('#checkbox').should('not.be.checked');
+        cy.get('#checkbox').check();
+        cy.get('#message').click();
+        cy.get('#message').click();
+        cy.get('body').click();
+        cy.get('#checkbox-example').click();
+        cy.get('#message').click();
+        cy.get('#message').should('have.text', 'It\'s back!');
+        /* ==== End Cypress Studio ==== */
+    });
+})
