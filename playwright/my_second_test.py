52778d78a27af0de6d330089efd8010db6d7a0c6
username960528 <username960528@gmail.com>
2023-08-29 13:15:59 +0500

    link_fix

--- playwright/my_second_test.py
+++ playwright/my_second_test.py
@@ -2,7 +2,7 @@ import re
 from playwright.sync_api import Page, expect
 
 def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(page: Page):
-    page.goto("http://127.0.0.1:9292/")
+    page.goto("http://the-internet.herokuapp.com/")
 
     expect(page).to_contain_text("A/B Testing")
 
