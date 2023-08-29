49e856637359a3dbbad3ff8e80596b050384b7cb
username960528 <username960528@gmail.com>
2023-08-29 13:46:47 +0500

    fix_links

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://the-internet.herokuapp.com/');
+  await page.goto('http://the-internet.herokuapp.com/'); 
   await page.getByRole('link', { name: 'A/B Testing' }).click();
   await expect(page.getByText('Also known as split testing. This is a way in which businesses are able to simul')).toHaveCount(1)
 });
